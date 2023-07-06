cff5061822761a234e88d4d4b69fb44b506aaa6a
kubagp <kubagp2@gmail.com>
2023-07-06 02:19:45 +0200

    style the dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,25 @@
-import { useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import { Padding } from "@mui/icons-material";
-import { Button, Container, Link, Paper, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import { Fragment } from "react";
 import { api } from "~/utils/api";
 import { Configuration } from "~/utils/configuration";
 import { Theme } from "~/utils/theme";
+import NextLink from "next/link";
 
 // const configurations: Configuration[] = [
 //   {
@@ -61,20 +77,41 @@ function Configurations() {
   if (isError) return <Typography>Error</Typography>;
 
   return data.map((cfg) => (
-    <Link href={`/dashboard/configuration/${cfg.id}`}>
-      <Paper
-        sx={{ width: 1 / 3, marginInline: "auto", p: 2, my: 1 }}
-        key={cfg.id}
-      >
-        <Typography variant="h6">{cfg.name}</Typography>
-        <Typography variant="body1">{cfg.id}</Typography>
-        <b>Base theme: </b> {cfg.baseTheme.name}
-        <br />
-        <b>Rules: </b> {cfg.rules.map((r) => r.name).join(", ")}
-        <br />
-        <b>Instances: </b> {cfg.instances.map((i) => i.name).join(", ")}
-      </Paper>
-    </Link>
+    <Fragment key={cfg.id}>
+      <Divider />
+      <Box sx={{ p: 2 }}>
+        <Link
+          component={NextLink}
+          href={`/dashboard/configuration/${cfg.id}`}
+          underline="hover"
+        >
+          <Typography variant="h6">{cfg.name}</Typography>
+        </Link>
+        <Box sx={{ mb: 1 }}></Box>
+        <table style={{ borderSpacing: "0px", minWidth: "50%" }}>
+          <tbody>
+            <tr>
+              <td>
+                <b>Base theme</b>
+              </td>
+              <td>{cfg.baseTheme.name}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Rules</b>
+              </td>
+              <td>{cfg.rules.map((r) => r.name).join(", ")}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Instances</b>
+              </td>
+              <td>{cfg.instances.map((i) => i.name).join(", ")}</td>
+            </tr>
+          </tbody>
+        </table>
+      </Box>
+    </Fragment>
   ));
 }
 
@@ -85,36 +122,98 @@ function Themes() {
   if (isError) return <Typography>Error</Typography>;
 
   return data.map((theme) => (
-    <Paper sx={{ width: 1 / 3, marginInline: "auto", p: 2 }} key={theme.id}>
-      <Typography variant="h6">{theme.name}</Typography>
-      <Typography variant="body1">{theme.id}</Typography>
-    </Paper>
+    <Fragment key={theme.id}>
+      <Divider />
+      <Box sx={{ p: 2 }}>
+        <Link
+          component={NextLink}
+          href={`/dashboard/theme/${theme.id}`}
+          underline="hover"
+        >
+          <Typography variant="h6">{theme.name}</Typography>
+        </Link>
+      </Box>
+    </Fragment>
   ));
 }
 
 export default function Dashboard() {
   return (
-    <Container>
-      <Typography variant="h4" sx={{ textAlign: "center" }}>
-        Dashboard
-      </Typography>
-      <Typography variant="h5" sx={{ textAlign: "center" }}>
-        Configurations
-      </Typography>
-      <Link
-        href="/dashboard/new-configuration"
-        sx={{ display: "block", mx: "auto" }}
-      >
-        <Button sx={{ mx: "auto", display: "block" }}>New configuration</Button>
-      </Link>
-      <Configurations />
-      <Typography variant="h5" sx={{ textAlign: "center" }}>
-        Themes
-      </Typography>
-      <Link href="/dashboard/new-theme" sx={{ display: "block", mx: "auto" }}>
-        <Button sx={{ mx: "auto", display: "block" }}>New Theme</Button>
-      </Link>
-      <Themes />
-    </Container>
+    <Box
+      sx={{
+        height: "100vh",
+        bgcolor: "#f5f5f5",
+      }}
+    >
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            school-clock Dashboard
+          </Typography>
+          <UserButton></UserButton>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Box>
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Stack sx={{ mt: 2 }}>
+                <Paper>
+                  <Typography variant="h5" sx={{ py: 1.5, p: 2 }}>
+                    Configurations
+                  </Typography>
+                  <Configurations />
+                  <Divider />
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <NextLink href="/dashboard/new-configuration">
+                      <Button sx={{ m: 2 }}>New configuration</Button>
+                    </NextLink>
+                  </Box>
+                </Paper>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Stack sx={{ mt: 2 }}>
+                <Paper>
+                  <Typography variant="h5" sx={{ py: 1.5, p: 2 }}>
+                    Themes
+                  </Typography>
+                  <Themes />
+                  <Divider />
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <NextLink href="/dashboard/new-theme">
+                      <Button sx={{ m: 2 }}>New theme</Button>
+                    </NextLink>
+                  </Box>
+                </Paper>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
+    // <Container>
+    //   <Typography variant="h4" sx={{ textAlign: "center" }}>
+    //     Dashboard
+    //   </Typography>
+    //   <Typography variant="h5" sx={{ textAlign: "center" }}>
+    //     Configurations
+    //   </Typography>
+    //   <Link
+    //     href="/dashboard/new-configuration"
+    //     sx={{ display: "block", mx: "auto" }}
+    //   >
+    //     <Button sx={{ mx: "auto", display: "block" }}>New configuration</Button>
+    //   </Link>
+    //   <Configurations />
+    //   <Typography variant="h5" sx={{ textAlign: "center" }}>
+    //     Themes
+    //   </Typography>
+    //   <Link href="/dashboard/new-theme" sx={{ display: "block", mx: "auto" }}>
+    //     <Button sx={{ mx: "auto", display: "block" }}>New Theme</Button>
+    //   </Link>
+    //   <Themes />
+    // </Container>
   );
 }
