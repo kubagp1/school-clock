5cff02a55975d479af79c0094d46f1c089e28178
kubagp <kubagp2@gmail.com>
2023-07-07 01:36:45 +0200

    add a link to dashboard

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,6 +1,14 @@
-import { UserButton } from "@clerk/nextjs";
-import { Box, AppBar, Toolbar, Typography, Container } from "@mui/material";
+import { UserButton, useUser } from "@clerk/nextjs";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from "@mui/material";
 import { ReactElement } from "react";
+import NextLink from "next/link";
 
 const DashboardLayout = ({ children }: { children: ReactElement }) => {
   return (
@@ -12,10 +20,14 @@ const DashboardLayout = ({ children }: { children: ReactElement }) => {
     >
       <AppBar sx={{ bgcolor: "#fefefe", color: "#010101" }} elevation={2}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            school-clock Dashboard
-          </Typography>
-          <UserButton></UserButton>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link component={NextLink} href="/dashboard" underline="none">
+              <Typography variant="h6" component="span">
+                school-clock Dashboard
+              </Typography>
+            </Link>
+          </Box>
+          <UserButton showName />
         </Toolbar>
       </AppBar>
       <Toolbar />
