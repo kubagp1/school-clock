9f1f59cffa227676308059b76e4face0a341979f
kubagp <kubagp2@gmail.com>
2023-08-04 03:22:53 +0200

    fix SelectTheme incorrect inputRef drilling

--- src/components/SelectTheme.tsx
+++ src/components/SelectTheme.tsx
@@ -7,9 +7,11 @@ type Props = React.ComponentProps<typeof Select>;
 export default forwardRef(function SelectTheme(props: Props, ref) {
   const { data, isLoading, isError } = api.theme.getAll.useQuery();
 
+  const notReadyProps = { ...props, inputRef: undefined };
+
   if (isLoading) {
     return (
-      <Select value="foo" key="loading" disabled {...props}>
+      <Select value="foo" key="loading" disabled {...notReadyProps}>
         {/* 'key' tells React to not reuse this Select */}
         <MenuItem value="foo">Loading...</MenuItem>
       </Select>
@@ -18,8 +20,8 @@ export default forwardRef(function SelectTheme(props: Props, ref) {
 
   if (isError) {
     return (
-      <Select value="foo" key="error" disabled {...props}>
-        <MenuItem value="foo">Error</MenuItem>
+      <Select value="foo" key="error" disabled {...notReadyProps}>
+        <MenuItem value="foo">Unable to load themes</MenuItem>
       </Select>
     );
   }
