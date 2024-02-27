9996dad5ed3b75607ec9ab5db6195cf95992885a
kubagp <kubagp2@gmail.com>
2024-02-27 19:39:28 +0100

    implement updateOrder mutation, add ownership checks

--- src/server/api/routers/rule.ts
+++ src/server/api/routers/rule.ts
@@ -46,6 +46,19 @@ export const ruleRouter = createTRPCRouter({
         });
       }
 
+      const themeCount = await ctx.prisma.theme.count({
+        where: {
+          id: input.themeId,
+          createdById: ctx.auth.userId,
+        },
+      });
+
+      if (themeCount === 0) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
       const largestIndex = await ctx.prisma.rule.findFirst({
         select: {
           index: true,
@@ -87,7 +100,20 @@ export const ruleRouter = createTRPCRouter({
         themeId: z.string().min(1),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
+      const themeCount = await ctx.prisma.theme.count({
+        where: {
+          id: input.themeId,
+          createdById: ctx.auth.userId,
+        },
+      });
+
+      if (themeCount === 0) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
       return ctx.prisma.rule.update({
         where: {
           id: input.id,
@@ -105,6 +131,78 @@ export const ruleRouter = createTRPCRouter({
           },
         },
       });
+    }),
+  updateOrder: privateProcedure
+    .input(
+      z.array(
+        z.object({
+          id: z.string().min(1),
+          index: z.number().min(0),
+        })
+      )
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (new Set(input.map((x) => x.id)).size !== input.length) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Duplicate rule IDs provided",
+        });
+      }
+
+      const currentRules = await ctx.prisma.rule.findMany({
+        where: {
+          id: {
+            in: input.map((x) => x.id),
+          },
+          configuration: {
+            createdById: ctx.auth.userId,
+          },
+        },
+      });
+
+      if (currentRules.length !== input.length) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message:
+            "Invalid rule IDs, or indexes for all rules in this configuration were not provided, or not all rules belong to the user",
+        });
+      }
+
+      const affectedRules = input.filter(
+        (rule) =>
+          rule.index !==
+          currentRules.find((currentRule) => currentRule.id === rule.id)?.index
+      );
+
+      const updatePromises = affectedRules.map((rule) =>
+        ctx.prisma.rule.update({
+          where: {
+            id: rule.id,
+          },
+          data: {
+            index: rule.index,
+          },
+        })
+      );
+
+      // Unique constraint on index would fail if we update without this
+      const intermediateUpdatePromises = affectedRules.map((rule) =>
+        ctx.prisma.rule.update({
+          where: {
+            id: rule.id,
+          },
+          data: {
+            index: -rule.index - 1, // -1 to avoid 0
+          },
+        })
+      );
+
+      await ctx.prisma.$transaction([
+        ...intermediateUpdatePromises,
+        ...updatePromises,
+      ]);
+
+      return;
     }),
   delete: privateProcedure.input(z.string().min(1)).query(({ ctx, input }) => {
     return ctx.prisma.rule.delete({
