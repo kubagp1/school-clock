de4b3c25959c7a985ef4968bfe2efb9f1fdb7a69
kubagp <kubagp2@gmail.com>
2024-03-19 23:41:29 +0100

    Add marquee auto-hiding after finish

--- src/components/client/NewsTicker.tsx
+++ src/components/client/NewsTicker.tsx
@@ -1,12 +1,24 @@
 import Marquee from "react-fast-marquee";
 import { useTheme } from "./ThemeProvider";
+import { use, useEffect, useState } from "react";
+import { set } from "zod";
 
 export function NewsTicker() {
   const theme = useTheme();
 
+  const [marqueeFinished, setMarqueeFinished] = useState(false);
+
+  const handleMarqueeFinish = () => {
+    setMarqueeFinished(true);
+  };
+
+  useEffect(() => {
+    setMarqueeFinished(false);
+  }, [theme.newsTickerText]);
+
   return (
     <div>
-      {theme.newsTickerText.length > 0 && (
+      {theme.newsTickerText.length > 0 && !marqueeFinished && (
         <Marquee
           key={theme.newsTickerText} // reset the marquee when the text changes
           speed={theme.newsTickerSpeed}
@@ -17,8 +29,10 @@ export function NewsTicker() {
             padding: "32px",
             backgroundColor: "#ddd",
           }}
+          className="marquee"
+          onFinish={handleMarqueeFinish}
         >
-          {theme.newsTickerText}
+          <span style={{ paddingLeft: "100vw" }}>{theme.newsTickerText}</span>
         </Marquee>
       )}
     </div>
