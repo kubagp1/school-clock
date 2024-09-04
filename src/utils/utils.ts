42484e0fb08d592eb295780e113968db58deedbb
kubagp <kubagp2@gmail.com>
2024-09-05 00:55:49 +0200

    move tw helper

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,10 @@
 export type Prettify<T> = {
   [K in keyof T]: T[K];
 } & unknown;
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
